870f73f2766f6cb0e4eafd02e0b64b8286e5a5bd
MothTheMortal <mohdmotha687@gmail.com>
2023-11-14 16:14:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Serve static files from the 'MyWebsite' directory
-app.use(express.static(path.join(__dirname, 'MyWebsite')));
+app.use(express.static(__dirname));
 
 // Define a route for your homepage
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'MyWebsite', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
