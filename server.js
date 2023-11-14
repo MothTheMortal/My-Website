2ca6d5267feace919a82d77e197681d00083ea4b
MothTheMortal <mohdmotha687@gmail.com>
2023-11-14 16:04:23 +0800

    Some more ffiles

---
+++ server.js
@@ -0,0 +1,38 @@
+// Example server.js
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+    const contentType = getContentType(filePath);
+
+    fs.readFile(filePath, 'utf8', (err, content) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('File not found');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content);
+        }
+    });
+});
+
+const getContentType = (filePath) => {
+    const extname = path.extname(filePath);
+    switch (extname) {
+        case '.html':
+            return 'text/html';
+        case '.css':
+            return 'text/css';
+        case '.js':
+            return 'text/javascript';
+        default:
+            return 'text/plain';
+    }
+};
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}/`);
+});
