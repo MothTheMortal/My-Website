dacc20c91ff0ad2da8d254a9576af91e96531d29
MothTheMortal <mohdmotha687@gmail.com>
2023-12-01 20:00:02 +0800

    Added search

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ const port = process.env.PORT || 3000;
 
 // Serve static files from the "styles" folder
 app.use('/styles', express.static(path.join(__dirname, 'styles')));
-
 // Serve resources (thumbnail, pfp, icon)
 app.use('/thumbnail', express.static(path.join(__dirname, 'thumbnail')));
 app.use('/pfp', express.static(path.join(__dirname, 'pfp')));
 app.use('/icon', express.static(path.join(__dirname, 'icon')));
-
+app.use(express.static(__dirname));
 // Serve the YouTube.html file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'youtube.html'));
@@ -20,3 +19,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
