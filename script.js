3c222f10802a89d5a8ec9fa61fa8612e55d5f572
MothTheMortal <mohdmotha687@gmail.com>
2023-12-01 20:05:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function searchYoutube() {
     const inputText = document.getElementById("search-input").value;
     const link = `https://www.youtube.com/results?search_query=${inputText}`;
     console.log(link);
-    window.open(link)
+    window.open(link, "_self")
 }
