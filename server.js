cf427ae58b2512fedbf78b532ec646688d09d0c5
MothTheMortal <mohdmotha687@gmail.com>
2023-12-01 16:58:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,19 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the 'MyWebsite' directory
-app.use(express.static(__dirname));
+// Serve static files from the "styles" folder
+app.use('/styles', express.static(path.join(__dirname, 'styles')));
 
-// Define a route for your homepage
+// Serve resources (thumbnail, pfp, icon)
+app.use('/thumbnail', express.static(path.join(__dirname, 'thumbnail')));
+app.use('/pfp', express.static(path.join(__dirname, 'pfp')));
+app.use('/icon', express.static(path.join(__dirname, 'icon')));
+
+// Serve the YouTube.html file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'youtube.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}/`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
